1489bca771f126d897c67b29daa5b8c1d684db11
Sam Tang <sam@iscool.tw>
2021-02-15 22:01:08 +0800

    Extract string Resource

--- app/src/main/java/com/samtang/guess/MainActivity.kt
+++ app/src/main/java/com/samtang/guess/MainActivity.kt
@@ -10,13 +10,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    //將SecretNubmer物件生成.
     val secretNumber = SecretNumber()
+    //固定TAG字串,以利後方的Log.d(可以代入) --手動輸入ManinActivity
+    //val TAG = "MainActivity"
+    //固定TAG字串,以利後方的Log.d(可以代入) --自動代入Class名稱
+    val TAG = MainActivity::class.java.simpleName
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //setContentView(R.layout.linear_main)
-        Log.d("MainActivity", "secret : ${secretNumber.secret}")
+        Log.d(TAG, "secret : ${secretNumber.secret}")
 
     }
 
@@ -24,15 +29,19 @@ class MainActivity : AppCompatActivity() {
     fun check(view: View){
         val n = number.text.toString().toInt()
         println("number : $n")
-        Log.d("MainActivity", "number : $n")
+        Log.d(TAG, "number : $n")
 
         val diff = secretNumber.validate(n)
-        var message = "Yes , You got it"
+        //var message = "Yes, You got it"
+        //將顯示的字串抽取(Extract string Resource)成資源檔(string.xml)內容
+        var message = getString(R.string.yes_you_get_it)
 
         if (diff < 0) {
-            message = "Bigger"
+            //message = "Bigger"
+            message = getString(R.string.bigger)
         } else if (diff > 0){
-            message = "Smaller"
+            //message = "Small"
+            message = getString(R.string.smaller)
         }
         //Toast 代入參數的 context 用 this , 最後要有.show() 才會顯示在畫面上
         //Toast.makeText(this,message, Toast.LENGTH_LONG).show()
@@ -40,9 +49,11 @@ class MainActivity : AppCompatActivity() {
         //AlertDialog.Builder(this).setTitle("Message").setMessage(message).setPositiveButton("OK",null).show()
         //產生訊息視窗以下為易讀的寫法
         AlertDialog.Builder(this)   //產生訊息視窗
-                .setTitle("Message")        //設定訊息視窗抬頭
+                //.setTitle("Message")      //設定訊息視窗抬頭
+                .setTitle(getString(R.string.dialog_title)) //將顯示的字串抽取(Extract string Resource)成資源檔(string.xml)內容
                 .setMessage(message)        //設定訊息視窗內文
-                .setPositiveButton("OK",null)   //設定訊息視窗內按鈕
+                //.setPositiveButton("OK",null)   //設定訊息視窗內按鈕
+                .setPositiveButton(getString(R.string.ok),null) //將顯示的字串抽取(Extract string Resource)成資源檔(string.xml)內容
                 .show()                     //顯示訊息視窗
 
         number.text.clear()
